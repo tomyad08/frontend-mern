2c006e606219e58dfaecd268fef29b76dd6d3791
LAPTOP-JF5K0512\tomyt <tomyad08@gmail.com>
2023-06-17 11:32:41 +0700

    ubah tampilan edit dan add data

--- src/Pages/editData.jsx
+++ src/Pages/editData.jsx
@@ -33,77 +33,78 @@ const EditData = () => {
   };
 
   return (
-    <div>
-      <div
-        className=" py-3 container-fluid"
-        style={{ backgroundColor: "#2c12bc" }}
-      >
-        <div
-          style={{ backgroundColor: "grey", width: "100px", height: "30px" }}
-        ></div>
-      </div>
-      <div className="mx-5">
-        <div
-          style={{
-            width: "100px",
-            height: "30px",
-            backgroundColor: "grey",
-            marginBottom: "30px",
-            marginTop: "20px",
-          }}
-        ></div>
-        <h1>Edit Data</h1>
-        <p>Silahkan perbaiki data diri mahasiswa di bawah ini:</p>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            onChange={(e) => setNama(e.target.value)}
-            placeholder={data.nama_lengkap}
-            className="p-1 border border-2 rounded-3 my-1 btn btn-light"
-            style={{ width: "20%" }}
-          />
-          <select
-            className="p-1 border border-2 rounded-3 my-1 btn btn-light"
-            onChange={(e) => setJenis(e.target.value)}
-            style={{ width: "20%" }}
-          >
-            <option value="Laki-laki">Laki-laki</option>
-            <option value="Perempuan">Perempuan</option>
-          </select>{" "}
-          <br />
-          <input
-            type="text"
-            onChange={(e) => setTanggal(e.target.value)}
-            placeholder={data.tanggal_lahir.substring(0, 10)}
-            className="p-1 border border-2 rounded-3 my-1 btn btn-light"
-            style={{ width: "20%" }}
-          />{" "}
-          <input
-            type="text"
-            onChange={(e) => setTempat(e.target.value)}
-            placeholder={data.tempat_lahir}
-            className="p-1 border border-2 rounded-3 my-1 btn btn-light"
-            style={{ width: "20%" }}
-          />{" "}
-          <br />
-          <input
-            type="text"
-            onChange={(e) => setAlamat(e.target.value)}
-            placeholder={data.alamat}
-            className="p-1 border border-2 rounded-3 my-1 btn btn-light"
-            style={{ width: "40%" }}
-          />
-          <br />
-          <Link to="/dashboard">
-            <button className=" btn btn-danger py-1 px-3 border border-0 rounded-1 me-2">
-              Cancel
-            </button>
-          </Link>
-          <input
-            type="submit"
-            className=" btn btn-success py-1 px-3 border border-0 rounded-1"
-          />
-        </form>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          <div
+            style={{
+              width: "100px",
+              height: "30px",
+              backgroundColor: "grey",
+              marginBottom: "30px",
+              marginTop: "20px",
+            }}
+          ></div>
+          <div className="mx-5 p-1 ">
+            <h1>Edit Data</h1>
+            <p>Silahkan perbaiki data diri mahasiswa di bawah ini:</p>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                onChange={(e) => setNama(e.target.value)}
+                placeholder={data.nama_lengkap}
+                className="p-1 border border-2 rounded-3 my-1 btn btn-light"
+                style={{ width: "100%" }}
+              />
+              <br />
+              <select
+                className="p-1 border border-2 rounded-3 my-1 btn btn-light"
+                onChange={(e) => setJenis(e.target.value)}
+                style={{ width: "100%" }}
+              >
+                <option value="Laki-laki">Laki-laki</option>
+                <option value="Perempuan">Perempuan</option>
+              </select>{" "}
+              <br />
+              <input
+                type="text"
+                onChange={(e) => setTanggal(e.target.value)}
+                placeholder={data.tanggal_lahir.substring(0, 10)}
+                className="p-1 border border-2 rounded-3 my-1 btn btn-light"
+                style={{ width: "100%" }}
+              />{" "}
+              <br />
+              <input
+                type="text"
+                onChange={(e) => setTempat(e.target.value)}
+                placeholder={data.tempat_lahir}
+                className="p-1 border border-2 rounded-3 my-1 btn btn-light"
+                style={{ width: "100%" }}
+              />{" "}
+              <br />
+              <input
+                type="text"
+                onChange={(e) => setAlamat(e.target.value)}
+                placeholder={data.alamat}
+                className="p-1 border border-2 rounded-3 my-1 btn btn-light"
+                style={{ width: "100%" }}
+              />
+              <br />
+              <input
+                type="submit"
+                className=" btn btn-light border-success py-1 px-5 border border-2 rounded-1 float-end mt-5"
+              />
+              <Link to="/dashboard">
+                <button className=" btn btn-light py-1 px-5 border border-2 border-danger rounded-1 mt-5">
+                  Cancel
+                </button>
+              </Link>
+            </form>
+          </div>
+        </div>
+        <div className="col container-fluid">
+          <img src="./hijab_work.jpg" alt=" " width="80%" />
+        </div>
       </div>
     </div>
   );
